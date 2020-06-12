29e0b7b3914704f8a9dee889a5366c2015a04f42
Luis Fernando Petreca <lfpetreca02@gmail.com>
2020-06-12 18:08:03 -0300

    sharing dumby data across requests

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const adminRoutes = require('./routes/admin')
+const rootDir = require('./utils/path')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+/* app.set('view engine', 'pug')
+app.set('views', 'views') */
 
-app.use('/admin/', adminRoutes)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use('/admin/', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 app.listen(3000)
