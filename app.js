c05f3b5be29aeb621e382f5fb12e68e56145699f
Luis Fernando Petreca <lfpetreca02@gmail.com>
2020-06-15 14:14:32 -0300

    add pages and ajust mobile nav menu

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const rootDir = require('./utils/path')
 const errorController = require('./controllers/error')
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(rootDir, 'public')))
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/admin/', adminRoutes)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(errorController.get404)
