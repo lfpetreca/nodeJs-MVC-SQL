44804d2d0c47c71c7251d7a7e948da160ead2f93
Luis Fernando Petreca <lfpetreca02@gmail.com>
2020-06-18 14:57:23 -0300

    edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,16 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productsCSS: true,
-        activeAddProduct: true
+        editing: false
     })
 }
 
 exports.postAddProduct = (req, res, next) => {
     const product = new Product(
+        null,
         req.body.title,
         req.body.imageUrl,
         req.body.description,
@@ -19,6 +18,41 @@ exports.postAddProduct = (req, res, next) => {
     )
     product.save()
     res.redirect('/')
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit
+
+    if (!editMode) {
+        return res.redirect('/')
+    }
+
+    const prodId = req.params.productId
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '',
+            editing: editMode,
+            product: product
+        })
+    })
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId
+    
+    const updateProduct = new Product(
+        prodId,
+        req.body.title,
+        req.body.imageUrl,
+        req.body.description,
+        req.body.price
+    )
+    updateProduct.save()
+    res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
